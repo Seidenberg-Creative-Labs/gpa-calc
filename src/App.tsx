a2c4e6034cb3361cf60e0221c0676b53dbc0171a
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-07-30 20:28:46 -0400

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import reactLogo from './assets/images/logo.svg';
-import './App.css';
+import reactLogo from "./assets/images/logo.svg";
+import "./App.css";
 
 const App = () => {
+
   return (
-    <div className="App">
-      <header className="header">
-        <img src={reactLogo} className="logo react" alt="logo" />
-        <p>GPA Calculator</p>
-      </header>
-      <body className="App-body">
-      </body>
-    </div>
-  );
+      <div className="App">
+          <div className="header">
+              <img id="pacelogo" src="src/assets/images/Pace_University_logo.png" alt="Pace University Logo" width={150}></img>
+              <h1 id="gpaHeader">GPA CALCULATOR</h1>
+              <img id="pacemascot" src="src/assets/images/Pace_University_Mascot.png" alt="Pace Univeristy Pace_University_Mascot" width={130}></img>                
+          </div>
+      </div>
+    );
 };
 
 export default App;
