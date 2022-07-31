70f563c7f742930605e97a44ddf7a05a8d131468
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-07-30 20:49:34 -0400

    Made header as a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import reactLogo from "./assets/images/logo.svg";
 import "./App.css";
+import WebHeader from "./components/WebHeader";
 
 const App = () => {
 
   return (
       <div className="App">
-          <div className="header">
-              <img id="pacelogo" src="src/assets/images/Pace_University_logo.png" alt="Pace University Logo" width={150}></img>
-              <h1 id="gpaHeader">GPA CALCULATOR</h1>
-              <img id="pacemascot" src="src/assets/images/Pace_University_Mascot.png" alt="Pace Univeristy Pace_University_Mascot" width={130}></img>                
-          </div>
+          <WebHeader/>
       </div>
     );
 };
