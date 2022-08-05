ae1c046c60b864a05c4049d1dc5f7791911d13b6
AlexAtPace <as23746n@pace.edu>
2022-08-04 23:31:33 -0400

    dropdown 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import reactLogo from './assets/images/logo.svg';
 import './App.css';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import Drop from './components/Drop';
 
 const App = () => {
+
   return (
     <div className="App">
       <header className="header">
-        <img src={reactLogo} className="logo react" alt="logo" />
         <p>GPA Calculator</p>
       </header>
       <body className="App-body">
+      <Drop/>
       </body>
     </div>
   );
+
 };
 
 export default App;
