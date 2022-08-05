7d0c19cb99de27de9ec54d1d631e720c92feb03b
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-08-05 15:01:50 -0400

    Merge branch 'main' of https://github.com/Seidenberg-Creative-Labs/gpa-calc

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import reactLogo from "./assets/images/logo.svg";
 import "./App.css";
 import 'react-dropdown/style.css';
 import WebHeader from "./components/WebHeader";
-import Drop from "./components/Drop"
 
 const App = () => {
 
   return (
       <div className="App">
           <WebHeader/>
-          <Drop/>
       </div>
     );
 };
