90dc0d0192bcebd1e5c55153f6f274ebb7492433
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-08-04 23:50:04 -0400

    Included Dropdown

---
+++ src/components/Drop.tsx
@@ -0,0 +1,23 @@
+import Dropdown from 'react-dropdown';
+import { useState } from 'react';
+
+const Drop = () => {
+
+const [option, setOption] = useState();
+
+  const options1 = [
+    'China', 'India'
+  ];
+  const options2 = [
+    'China System', 'India System'
+  ];
+    return(
+      <div>
+      <Dropdown options={options1}  placeholder="Country" />
+      <Dropdown options={options2}  placeholder="GPA System" />
+      </div>
+    );
+
+}
+
+export default Drop;
