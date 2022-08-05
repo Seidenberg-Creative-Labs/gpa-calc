bdf1489c605c961daa98c58fd714b8638e2f0aed
AlexAtPace <as23746n@pace.edu>
2022-08-04 23:31:33 -0400

    dropdown 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ import WebHeader from "./components/WebHeader";
 import Drop from "./components/Drop";
 
 const App = () => {
-  const [courseTableData, setCourseTableData] = useState([
-      {id: 1, courseTitle: null, credits: null, grade: null},
-      {id: 2, courseTitle: null, credits: null, grade: null},
-      {id: 3, courseTitle: null, credits: null, grade: null}
-  ]);
 
   return (
     <div className="App">
@@ -26,9 +21,15 @@ const App = () => {
             <Button radius={9} color='#fafafa' text='Calculate GPA' width='150px' height='45px' onClick={() => {}} />
             <Button radius={9} color='#fafafa' text='Clear Table' width='150px' height='45px' onClick={() => {}} />
         </div>
+
+        <p>GPA Calculator</p>
+      </header>
+      <body className="App-body">
+      <Drop/>
       </body>
     </div>
   );
+
 };
 
 export default App;
