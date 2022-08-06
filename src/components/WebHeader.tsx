ddf0b8ab6642de64a863de053c35a3991ed8c704
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-08-05 20:26:39 -0400

    Changed and adjusted pace logos

--- src/components/WebHeader.tsx
+++ src/components/WebHeader.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const WebHeader = () => {
     return(
         <div className="pageHeader">
-              <img id="pacelogo" src="src/assets/images/Pace_University_logo.png" alt="Pace University Logo" width={150}></img>
+              <img id="paceSCIS" src="src/assets/images/Pace_SEIDENBERG.png" alt="Pace University Logo" ></img>
               <h1 id="gpaHeader">GPA CALCULATOR</h1>
-              <img id="pacemascot" src="src/assets/images/Pace_University_Mascot.png" alt="Pace Univeristy Pace_University_Mascot" width={130}></img>                
+              <img id="paceOoA" src="src/assets/images/Pace-OfficeofAdmission.png" alt="Pace Univeristy Pace_University_Mascot" ></img>                
         </div>
         );
 }
