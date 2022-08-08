2f6a3c5a01653b18a3680d5d179c3c8f27f23f3a
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-08-08 17:47:43 -0400

    Fixed header looks

--- src/components/WebHeader.tsx
+++ src/components/WebHeader.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const WebHeader = () => {
     return(
-        <div className="pageHeader">
+        <div className="header">
               <img id="paceSCIS" src="src/assets/images/Pace_SEIDENBERG.png" alt="Pace University Logo" ></img>
-              <h1 id="gpaHeader">GPA CALCULATOR</h1>
+              <h1 id="gpaHeader" >GPA CALCULATOR</h1>
               <img id="paceOoA" src="src/assets/images/Pace-OfficeofAdmission.png" alt="Pace Univeristy Pace_University_Mascot" ></img>                
         </div>
         );
