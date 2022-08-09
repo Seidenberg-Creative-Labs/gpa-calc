398418f9bc35c08d116641979e45a4d03a21b33f
AlexAtPace <as23746n@pace.edu>
2022-08-08 22:09:42 -0400

    fixing alex's branch

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,6 @@ const App = () => {
       {id: 3, courseTitle: null, credits: null, grade: null}
   ]);
 
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import Drop from './components/Drop';
-
-const App = () => {
-
-
   return (
     <div className="App">
       <header className="header">
@@ -33,14 +26,9 @@ const App = () => {
             <Button radius={9} color='#fafafa' text='Calculate GPA' width='150px' height='45px' onClick={() => {}} />
             <Button radius={9} color='#fafafa' text='Clear Table' width='150px' height='45px' onClick={() => {}} />
         </div>
-        <p>GPA Calculator</p>
-      </header>
-      <body className="App-body">
-      <Drop/>
       </body>
     </div>
   );
-
 };
 
 export default App;
