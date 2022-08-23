065693268452abae149d789c664be0e2c2f409db
Michael Dadi <mdadi22@gmail.com>
2022-08-23 02:01:44 -0400

    Updated App.tsx with setData functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import './App.css';
 import TableCourseInput from "./components/tables/TableCourseInput";
 import Button from "./components/Button";
 import "react-dropdown/style.css";
-import WebHeader from "./components/WebHeader";
 import Drop from "./components/Drop";
 
 const App = () => {
   const [data, setData] = useState([
-      {id: 1, courseTitle: null, credits: null, grade: null},
-      {id: 2, courseTitle: null, credits: null, grade: null},
-      {id: 3, courseTitle: null, credits: null, grade: null}
+      ["", "1", "0"],
+      ["", "1", "0"],
+      ["", "1", "0"],
   ]);
 
   return (
@@ -22,12 +21,22 @@ const App = () => {
       </header>
       <body className="body">
       {/*@ts-ignore*/}
-      <Drop/>
+      <Drop />
         <TableCourseInput data={data} setData={setData} />
         <div id='btnMenu'>
-            <Button radius={9} color='#fafafa' text='Add Course' width='150px' height='45px' onClick={() => {}} />
-            <Button radius={9} color='#fafafa' text='Calculate GPA' width='150px' height='45px' onClick={() => {}} />
-            <Button radius={9} color='#fafafa' text='Clear Table' width='150px' height='45px' onClick={() => {}} />
+            <Button radius={9} color='#fafafa' text='Add Course' width='150px' height='45px' onClick={() => {
+                setData([...data, ["", "1", "0"]])
+            }} />
+            <Button radius={9} color='#fafafa' text='Calculate GPA' width='150px' height='45px' onClick={() => {
+                alert("TODO: Implement this function\n\n" + JSON.stringify(data))
+            }} />
+            <Button radius={9} color='#fafafa' text='Clear Table' width='150px' height='45px' onClick={() => {
+                setData([
+                    ["", "1", "0"],
+                    ["", "1", "0"],
+                    ["", "1", "0"],
+                ])
+            }} />
         </div>
       </body>
     </div>
