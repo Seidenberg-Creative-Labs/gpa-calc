8eee1329b6ab0d9d0488edaac03d6ca55eb8e9c0
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-08-25 16:29:43 -0400

    format code

--- src/components/GpaDrop.tsx
+++ src/components/GpaDrop.tsx
@@ -1,35 +1,21 @@
-import Dropdown from 'react-dropdown';
+import Dropdown from "react-dropdown";
 
-const GpaDrop = ({country}:any) => {
+const GpaDrop = ({ country }: any) => {
+    const ChinaScale = ["5 Point Scale", "4 Point Scale"];
 
+    const IndiaScale = [
+        "Most Common Scale",
+        "Letter Grade Scale",
+        "10 Point Scale",
+    ];
 
-  const ChinaScale = [
-    "5 Point Scale", "4 Point Scale"
-  ];
-
-  const IndiaScale = [
-    'Most Common Scale', 'Letter Grade Scale', '10 Point Scale'
-  ];
-
-  if (country === 'China'){
-    return(
-        <Dropdown options={ChinaScale} value={"Select China Scale"} />
-    );
-}
-
-else if (country === 'India'){
-    return(
-      <Dropdown options={IndiaScale} value={"Select India Scale"} />
-    );
-}
-
-else{
-    return(
-        <Dropdown options={[]} placeholder = "Select a Country" />
-    );
-}
-
-
-}
+    if (country === "China") {
+        return <Dropdown options={ChinaScale} value={"Select China Scale"} />;
+    } else if (country === "India") {
+        return <Dropdown options={IndiaScale} value={"Select India Scale"} />;
+    } else {
+        return <Dropdown options={[]} placeholder="Select a Country" />;
+    }
+};
 
 export default GpaDrop;
