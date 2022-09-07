ce918acb2d6a9a5e1eda34192408247f49ab3065
Michael Dadi <mdadi22@gmail.com>
2022-09-06 20:17:03 -0400

    CSSTransition animation for output table

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import TableGradesOutput from "./components/tables/TableGradesOutput";
 import {placeHolder} from "./components/dropdown/GpaDrop";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.min.css';
+import {CSSTransition} from "react-transition-group";
 
 const App = () => {
 
@@ -79,10 +80,16 @@ const App = () => {
                     ["", "1", "0"],
                 ]);
                 // Timeout to allow state to update
-                setTimeout(() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'}), 10);
+                // setTimeout(() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'}), 10);
             }}>Reset Table</Button>
         </Stack>
-      {showOutput && <TableGradesOutput data={getGradeOutput(placeHolder, data)} />}
+      <CSSTransition
+          in={showOutput}
+          timeout={300}
+          classNames="fade"
+          unmountOnExit>
+        <TableGradesOutput data={getGradeOutput(placeHolder, data)} />
+      </CSSTransition>
       <ToastContainer position="bottom-center" />
       </body>
     </div>
