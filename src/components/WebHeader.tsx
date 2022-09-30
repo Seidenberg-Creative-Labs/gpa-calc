4c8bf7fa046a732874f0d131acfe8163d15f97e7
Michael Dadi <mdadi22@gmail.com>
2022-09-30 09:11:14 -0400

    Update header and replace assets folder with public

--- src/components/WebHeader.tsx
+++ src/components/WebHeader.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const WebHeader = () => {
     return(
         <div className="header">
-              <img id="paceSCIS" src="/images/Pace_SEIDENBERG.png" alt="Pace University Logo" ></img>
+              <img id="paceSCIS" src="./Pace_SEIDENBERG.png" alt="Pace University Logo" ></img>
               <h1 id="gpaHeader" >GPA CALCULATOR</h1>
-              <img id="paceOoA" src="/images/Pace-OfficeofAdmission.png" alt="Pace Univeristy Pace_University_Mascot" ></img>
+              <img id="paceOoA" src="./Pace-OfficeofAdmission.png" alt="Pace Univeristy Pace_University_Mascot" ></img>
         </div>
         );
 }
