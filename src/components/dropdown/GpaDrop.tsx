6519ba9b91f1e665f76c5bf627e6ca74ee8abc9c
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2022-11-13 18:26:21 -0500

    Dropdown fixing and styling

--- src/components/dropdown/GpaDrop.tsx
+++ src/components/dropdown/GpaDrop.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { FormControl, InputLabel, MenuItem } from "@mui/material";
 
 let getScale: string;
 // Country Drop Down
-const GpaDrop = ({ country }: any) => {
+const GpaDrop = (props: any) => {
     // Setting the Scale
     const [scale, setScale] = useState('');
-    // Exporting the Scale Value
+
+    // Updating export value
     const handleChange = (e: SelectChangeEvent) => {
-        setScale(e.target.value);
-        getScale = e.target.value;
-    };
+        setScale(e.target.value)
+        getScale = e.target.value
+    }
+
+    //Hides India scale when Country isn't India
+    const indiaHidden = () => {
+        if(props.country === 'China')
+            return true
+        return false
+    }
+
+    //Hides China scale when Country isn't China
+    const chinaHidden = () => {
+        if(props.country === 'India')
+            return true
+        return false
+    }
+    
+    
     return(
-    <FormControl size="small" sx={{ width: 160 }}>
+    <FormControl size="small" sx={{ width: 'auto', minWidth: 146}}>
                 <InputLabel>Grading Scale</InputLabel>
                 <Select
                     defaultValue={''}
@@ -21,15 +38,17 @@ const GpaDrop = ({ country }: any) => {
                     value={scale}
                     onChange={handleChange}
                     label={'Grading Scale'}
+                    autoWidth={true}
                 >
+                    {/* India Scales */}
+                    <MenuItem hidden={indiaHidden()} value={"Most Common Scale"}>Most Common Scale</MenuItem>  
+                    <MenuItem hidden={indiaHidden()} value={"Letter Grade Scale"}>Letter Grade Scale</MenuItem>  
+                    <MenuItem hidden={indiaHidden()} value={"Choice Based System"}>Choice Based Credit Scale</MenuItem>  
+                    <MenuItem hidden={indiaHidden()} value={"IIT Scale"}> IIT Scale</MenuItem> 
                     {/* China Scales */}
-                    {country === 'China' ? <MenuItem value={"5 Point Scale"}>5 Point Scale</MenuItem> : null}
-                    {country === 'China' ? <MenuItem value={"4 Point Scale"}>4 Point Scale</MenuItem> : null}
-                    {/* Indian Scales */}
-                    {country === 'India' ? <MenuItem value={"Most Common Scale"}>Most Common Scale</MenuItem> : null}
-                    {country === 'India' ? <MenuItem value={"Letter Grade Scale"}>Letter Grade Scale</MenuItem> : null}
-                    {country === 'India' ? <MenuItem value={"Choice Based System"}>Choice Based Credit Scale</MenuItem> : null}
-                    {country == 'India' ? <MenuItem value={"IIT Scale"}> IIT Scale</MenuItem>: null};
+                    <MenuItem hidden={chinaHidden()} value={"5 Point Scale"}>5 Point Scale</MenuItem>  
+                    <MenuItem hidden={chinaHidden()} value={"4 Point Scale"}>4 Point Scale</MenuItem>  
+
                 </Select>
             </FormControl>
     );
